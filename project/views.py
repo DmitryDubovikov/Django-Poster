4120608d91113a302ce1090fec122462b2dedfbc
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-04 15:35:54 -0300

    now data is taken from DB, not from json files

--- project/views.py
+++ project/views.py
@@ -1,32 +1,36 @@
+import copy
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place
 
 
 def index(request):
     template = loader.get_template("index.html")
-    places_data = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {"type": "Point", "coordinates": [37.62, 55.793676]},
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "static/places/moscow_legends.json",
-                },
-            },
-            {
-                "type": "Feature",
-                "geometry": {"type": "Point", "coordinates": [37.64, 55.753676]},
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "static/places/roofs24.json",
-                },
-            },
-        ],
+
+    features = []
+    i = 0
+    feature_template = {
+        "type": "Feature",
+        "geometry": {"type": "Point", "coordinates": [1, 1]},
+        "properties": {
+            "title": "title",
+            "placeId": "placeId",
+            "detailsUrl": "detailsUrl",
+        },
     }
+
+    places = Place.objects.all()
+
+    for place in places:
+        i += 1
+        new_feature = copy.deepcopy(feature_template)
+        new_feature["geometry"]["coordinates"][0] = place.lng
+        new_feature["geometry"]["coordinates"][1] = place.lat
+        new_feature["properties"]["title"] = place.title
+        new_feature["properties"]["placeId"] = "placeId" + str(i)
+        features.append(new_feature)
+
+    places_data = {"type": "FeatureCollection", "features": features}
     context = {"places_data": places_data}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
