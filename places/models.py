f5cbf102a6c795cf368617a8616869844a87f6ac
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-04 16:17:29 +0300

    add model for images

--- places/models.py
+++ places/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
 
-# Create your models here.
 class Place(models.Model):
     title = models.CharField(max_length=255)
     description_short = models.TextField()
     description_long = models.TextField()
     lng = models.FloatField()
     lat = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
