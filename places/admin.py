7ec92bf9848facb5e325e8529b58d864c18bf118
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-04 21:07:16 -0300

    step 14: previews in admin site in Images

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 from .models import Place, Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    readonly_fields = ["image_preview"]
+
+    def image_preview(self, obj):
+        return format_html(
+            '<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.image.url,
+                width=200,
+                height=200,
+            )
+        )
 
 
 class ImageInline(admin.TabularInline):
